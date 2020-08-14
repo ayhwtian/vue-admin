7cafe5c26cd3d7fb62e4c526696c2faf5d62c8f9
Honwei <ayhwtian@163.com>
2020-08-14 18:58:05 +0800

    login登陆页布局验证

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/plugins/element.js'
+
+// 导入全局样式表
+import '@/assets/css/base.css'
 
 Vue.config.productionTip = false
+
+
 
 new Vue({
   router,
